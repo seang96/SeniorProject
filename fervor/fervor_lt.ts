f845d212974f5ddb7cb1b20e65f1a8d2d7cc59b3
seang96 <seang96@spgrn.com>
2014-03-01 22:32:20 -0500

    Added Fervor Files
    Needed for updater

---
+++ fervor/fervor_lt.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt_LT">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="23"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="46"/>
+        <source>Information</source>
+        <translation type="unfinished">Informacija</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="67"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;This project was made to showcase all of my previous work.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;The project overall was difficult and taught me a lot about programing. I learned about functions, multiple windows (objects), translations, animations, and cleaner code.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Verison&lt;/span&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt; &lt;/span&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;1.0.0&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:7pt;&quot;&gt;Copyright © 2014, Sean Greenawalt (ryuk67@gmail.com)&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="95"/>
+        <source>SPL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="101"/>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="114"/>
+        <source>qrc:/html/html/credits.html</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="121"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="134"/>
+        <source>qrc:/html/html/license.html</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="150"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="163"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FvUpdateConfirmDialog</name>
+    <message>
+        <location filename="fvupdateconfirmdialog.ui" line="17"/>
+        <source>Software Update</source>
+        <oldsource>Update</oldsource>
+        <translation>Programos naujinimas</translation>
+    </message>
+    <message>
+        <location filename="fvupdateconfirmdialog.ui" line="23"/>
+        <source>The update file is located at:</source>
+        <translation>Naujinio adresas:</translation>
+    </message>
+    <message>
+        <location filename="fvupdateconfirmdialog.ui" line="30"/>
+        <source>&lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="fvupdateconfirmdialog.ui" line="43"/>
+        <source>Download this update, close &quot;%1&quot;, install it, and then reopen &quot;%1&quot;.</source>
+        <translation>Atsisiųskite naujinį, išjunkite %1, įdiekite naujinį ir vėl paleiskite %1.</translation>
+    </message>
+    <message>
+        <location filename="fvupdateconfirmdialog.ui" line="50"/>
+        <source>When you click &quot;OK&quot;, this link will be opened in your browser.</source>
+        <translation>Jei paspauskite „Gerai“, naujinio nuoroda atsidarys Jūsų naršyklėje.</translation>
+    </message>
+</context>
+<context>
+    <name>FvUpdateWindow</name>
+    <message>
+        <location filename="fvupdatewindow.ui" line="17"/>
+        <source>Software Update</source>
+        <translation>Naujinimas</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="31"/>
+        <source>A new version of %1 is available!</source>
+        <oldsource>A new version of %s is available!</oldsource>
+        <translation>Išleista nauja „%1“ versija!</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="38"/>
+        <source>%1 %2 is now available - you have %3. Would you like to download it now?</source>
+        <oldsource>%s %s is now available - you have %s. Would you like to download it now?</oldsource>
+        <translation>Išleista nauja %1 versija %2. Jūs naudojate %3. Ar norėtumėte atnaujinti?</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="51"/>
+        <source>Release Notes:</source>
+        <translation>Atnaujinta:</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="64"/>
+        <source>about:blank</source>
+        <translation>about:blank</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="77"/>
+        <source>Skip This Version</source>
+        <translation>Praleisti šį naujinį</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="97"/>
+        <source>Remind Me Later</source>
+        <translation>Priminti vėliau</translation>
+    </message>
+    <message>
+        <location filename="fvupdatewindow.ui" line="104"/>
+        <source>Install Update</source>
+        <translation>Įdiegti naujinį</translation>
+    </message>
+</context>
+<context>
+    <name>FvUpdater</name>
+    <message>
+        <location filename="fvupdater.cpp" line="221"/>
+        <source>Unable to open this link in a browser. Please do it manually.</source>
+        <translation>Nepavyko atidaryti nuorodos naršyklėje. Pabandykite nuorodą nukopijuoti ir atidaryti pats.</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="343"/>
+        <source>Feed download failed: %1.</source>
+        <translation>Nepavyko atsisiųsti informacijos apie naujinį: %1.</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="482"/>
+        <source>Feed parsing failed: %1 %2.</source>
+        <translation>Nepavyko perskaityti informacijos apie naujinį: %1 %2.</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="519"/>
+        <source>Feed error: &quot;release notes&quot; link is empty</source>
+        <translation>Nepavyko rasti nuorodos į pastabas apie naujinį</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="528"/>
+        <source>Feed error: invalid &quot;release notes&quot; link</source>
+        <translation>Nekorektiška nuoroda į pastabas apie naujinį</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="532"/>
+        <source>Feed error: invalid &quot;enclosure&quot; with the download link</source>
+        <translation>Nepavyko rasti nuorodos į failą su naujiniu</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="490"/>
+        <location filename="fvupdater.cpp" line="540"/>
+        <source>No updates were found.</source>
+        <translation>Naujinių nėra.</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="582"/>
+        <source>Error</source>
+        <translation>Klaida</translation>
+    </message>
+    <message>
+        <location filename="fvupdater.cpp" line="598"/>
+        <source>Information</source>
+        <translation>Informacija</translation>
+    </message>
+</context>
+<context>
+    <name>SeniorProject</name>
+    <message>
+        <location filename="../seniorproject.ui" line="283"/>
+        <source>Programs:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="337"/>
+        <source>Miscellaneous:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="589"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="609"/>
+        <source>Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="814"/>
+        <location filename="../seniorproject.ui" line="1112"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="864"/>
+        <source>Select your language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="883"/>
+        <source>Update the program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="907"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="931"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="950"/>
+        <source>Select your theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="1021"/>
+        <source>Install/Update Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="1045"/>
+        <source>Check for updates on start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../seniorproject.ui" line="1069"/>
+        <source>Reset Updater</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../program_buttons.cpp" line="53"/>
+        <source>Install MCUHelper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../program_buttons.cpp" line="84"/>
+        <source>Run FizzBuzz Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../videoplayer_controls.cpp" line="110"/>
+        <source>Video (Loading...)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../videoplayer_controls.cpp" line="117"/>
+        <source>Error. Could not load video: No Internet Connection?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../videoplayer_controls.cpp" line="122"/>
+        <location filename="../seniorproject.cpp" line="56"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="65"/>
+        <source>Open Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="75"/>
+        <source>Folder Not Found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="75"/>
+        <source>The folder specified cannot be found. Please recheck the folder&apos;s path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="80"/>
+        <source>File Not Found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="81"/>
+        <source>The folder specified does not contain the </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="82"/>
+        <source>core theme file &quot;theme.style&quot;. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../theming.cpp" line="83"/>
+        <source>Please recheck the folder&apos;s path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>select_language</name>
+    <message>
+        <location filename="../select_language.ui" line="20"/>
+        <source>Select Default Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../select_language.ui" line="37"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../select_language.ui" line="50"/>
+        <source>Please choose your language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
